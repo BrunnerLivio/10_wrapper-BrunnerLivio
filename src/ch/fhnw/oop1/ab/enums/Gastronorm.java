9a0dde738bfd8d46855a4b6de2b9af657000ef91
Livio <livio.brunner.lb1@gmail.com>
2022-11-15 22:15:32 +0100

    add gastronorm

--- src/ch/fhnw/oop1/ab/enums/Gastronorm.java
+++ src/ch/fhnw/oop1/ab/enums/Gastronorm.java
@@ -1,31 +1,54 @@
 package ch.fhnw.oop1.ab.enums;
 
 public class Gastronorm {
-    // private Size size;
-    // private Material material;
+  private Size size;
+  private Material material;
 
-    // public Gastronorm(Material material, Size size) {
-    //     this.material = material;
-    //     this.size = size;
-    // }
+  public Gastronorm(Material material, Size size) {
+    this.material = material;
+    this.size = size;
+  }
 
-    // public Material getMaterial() {
-    //     return material;
-    // }
+  public Material getMaterial() {
+    return material;
+  }
 
-    // public int getWidth() {
-    //     return size.getWidth();
-    // }
+  public int getWidth() {
+    return size.getWidth();
+  }
 
-    // public int getLength() {
-    //     return size.getLength();
-    // }
+  public int getLength() {
+    return size.getLength();
+  }
 
-    // public int getLiterVolume() {
-    //     return size.getWidth() * size.getLength() / 10_000;
-    // }
+  public int getLiterVolume() {
+    return size.getWidth() * size.getLength() / 10_000;
+  }
 
-    // TODO: Enum Material
+  enum Material {
+    STEEL,
+    PLASTIC
+  }
 
-    // TODO: Enum Size, all measurements in millimeters
+
+  enum Size {
+    GN1_4(163, 265), GN1_2(265, 326), GN1_1(325, 530);
+
+    private int breite;
+    private int laenge;
+
+    Size(int breite, int laenge) {
+      this.breite = breite;
+      this.laenge = laenge;
+    }
+
+    public int getWidth() {
+      return breite;
+    }
+
+    public int getLength() {
+      return laenge;
+    }
+
+  }
 }
